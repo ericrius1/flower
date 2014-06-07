b795710cd6ccae0f724aac83822d457277c49ba6
Eric <Ericrius1@gmail.com>
2014-06-07 16:00:07 +0200

    yar

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
-var camera, renderer, scene, controls, clock, perlin;
+//LIMITATIONS: Only HSL (no rgb)
+//preserve drawingbuffer must be on! (static painting at end)
+
+var camera, renderer, scene, controls, clock, perlin, wineglass= {};
 
 var line;
 var randFloat = THREE.Math.randFloat;
@@ -63,19 +66,30 @@ function animate() {
 }
 
 function createWineGlass(){
+  //save important info about this glass to be used to paint it later
   var p,y;
   var glassGeo = new THREE.Geometry();
+  var startingX = -25;
+  var endingX = 25;
+  var middleX = (endingX + startingX)/2;
   var func = function(x){
     return Math.pow(x,2)/20 - 40;
   }
-  for(var x = -25; x < 25; x+=.1){
+  for(var x = startingX; x < endingX; x+=.1){
+    //We need to save bottom of the glass
     y = func(x);
+    if( Math.floor(x) === middleX){
+      wineglass.bottom = y;
+    }
     p = new THREE.Vector3(x, y, 0);
     glassGeo.vertices.push(p);
   }
+  //Save the top of the glass
+  wineglass.top = p.y; 
   var wineMat = new THREE.LineBasicMaterial({color: new THREE.Color(0xff00ff), linewidth: 5})
   var wineGlass = new THREE.Line(glassGeo, wineMat);
   scene.add(wineGlass);
+  console.log(wineglass)
 }
 
 function stroke(){
