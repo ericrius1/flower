4c7a0a43120804936e52c7b9c8b7d2f4468eb718
Eric <Ericrius1@gmail.com>
2014-06-08 15:19:35 +0200

    tiast

--- js/landscape.js
+++ js/landscape.js
@@ -16,14 +16,14 @@ var Landscape = function(){
       y: topScreen
     };
     var strokeTween = new TWEEN.Tween(csd).
-      to(fsd, 3000).
+      to(fsd, 500).
       onUpdate(function(){
         brush.material.color = _.sample(brush.colors);
         brush.position.set(currentX, csd.y, 0)
       }).start();
       
       strokeTween.onComplete(function(){
-        currentX+=1
+        currentX+=5
         stroke();
       });
   }
